859ed872969c0d58830a051e625904a984e40a07
DeWard <ejtagaca@gmail.com>
2018-01-30 11:26:24 -0800

    Dropdown now passes ids to aStar. Locations inaccurate?

--- DrawMap/js/dropdown.js
+++ DrawMap/js/dropdown.js
@@ -3,25 +3,33 @@ var marketlist = ["",];
 var activitieslist = ["",];
 var classroomlist = ["",];
 var collectedAlready = false;
+var checkTo = false;
+var checkFrom = false;
+var toID;
+var fromID;
+var optionFromArr;
+var optionToArr;
+
 function collectData(){
    if (collectedAlready === false){
       for(i = 0; i < nodes.length; i++){
          if(nodes[i].type == "College"){
-            collegelist.push(nodes[i].name);
+            collegelist.push(nodes[i]);
          }
          if(nodes[i].type == "Market"){
-            marketlist.push(nodes[i].name);
+            marketlist.push(nodes[i]);
          }
          if(nodes[i].type == "Activities"){
-            activitieslist.push(nodes[i].name);
+            activitieslist.push(nodes[i]);
          }
          if(nodes[i].type == "Classroom"){
-            classroomlist.push(nodes[i].name);
+            classroomlist.push(nodes[i]);
          }
       }
       collectedAlready = true;
    }
 }
+
 function optfromquery(initfrom, dynafrom){
    collectData();
 
@@ -31,28 +39,29 @@ function optfromquery(initfrom, dynafrom){
    dynamic.innerHTML ="";
    
    if(initial.value == "College"){
-      var optionarr = collegelist;
+      optionFromArr = collegelist;
    }
    if(initial.value == "Market"){
-      var optionarr = marketlist;
+      optionFromArr = marketlist;
    }
    if(initial.value == "Activities"){
-      var optionarr = activitieslist;
+      optionFromArr = activitieslist;
    }
    if(initial.value == "Classroom"){
-      var optionarr = classroomlist;
+      optionFromArr = classroomlist;
    }
    
-   for(i = 0; i < optionarr.length; i++){
+   for(i = 0; i < optionFromArr.length; i++){
       var newOption = document.createElement("option");
-      newOption.value = optionarr[i].toLowerCase;
-      newOption.innerHTML = optionarr[i];
+      newOption.value = optionFromArr[i].name;
+      newOption.innerHTML = optionFromArr[i].name;
       dynamic.options.add(newOption);
    }
    
 }
 
 function opttoquery(initto, dynato){
+   
    collectData();
 
    var initial = document.getElementById(initto);
@@ -61,23 +70,47 @@ function opttoquery(initto, dynato){
    dynamic.innerHTML ="";
    
    if(initial.value == "College"){
-      var optionarr = collegelist;
+      optionToArr = collegelist;
    }
    if(initial.value == "Market"){
-      var optionarr = marketlist;
+      optionToArr = marketlist;
    }
    if(initial.value == "Activities"){
-      var optionarr = activitieslist;
+      optionToArr = activitieslist;
    }
    if(initial.value == "Classroom"){
-      var optionarr = classroomlist;
+      optionToArr = classroomlist;
    }
    
-   for(i = 0; i < optionarr.length; i++){
+   for(i = 0; i < optionToArr.length; i++){
       var newOption = document.createElement("option");
-      newOption.value = optionarr[i].toLowerCase;
-      newOption.innerHTML = optionarr[i];
+      newOption.value = optionToArr[i].name;
+      newOption.innerHTML = optionToArr[i].name;
       dynamic.options.add(newOption);
    }
    
+}
+
+function checkDropDownTo(dropdownopt){
+   optTo = document.getElementById(dropdownopt);
+   for(i = 0; i < optionToArr.length; i++){
+      if(optTo.value == optionToArr[i].name){
+         toID = optionToArr[i].id;
+      }
+   }
+   if (fromID != undefined && toID != undefined){
+      aStar(fromID, toID);
+   }
+}
+
+function checkDropDownFrom(dropdownopt){
+   optFrom = document.getElementById(dropdownopt);
+   for(i = 0; i < optionFromArr.length; i++){
+      if(optFrom.value == optionFromArr[i].name){
+         fromID = optionFromArr[i].id;
+      }
+   }
+   if (fromID != undefined && toID != undefined){
+      aStar(fromID, toID);
+   }
 }
--- DrawMap/js/runAstar.js
+++ DrawMap/js/runAstar.js
@@ -1,8 +1,10 @@
 var pathcheck = [];
-function aStar(){
+function aStar(optFrom, optTo){
+   pathcheck.push(optFrom);
    for(i=0; i < 30; i++){
       curr = Math.floor(Math.random() * nodes.length); 
       pathcheck.push(nodes[curr].id);
    }
+   pathcheck.push(optTo);
    clearMap(pathcheck);
 }
