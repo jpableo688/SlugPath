8efa1603be0c22f248953068891a465cbf404391
DeWard <ejtagaca@gmail.com>
2018-02-20 16:58:00 -0800

    fixing merge conflicts. deleted empty file

--- DrawMap/js/crownlocations.js
+++
@@ -1,8 +0,0 @@
-//this is an array of nodes objects in json.
-//I already loaded it into nodes and saved it as
-//js because we would have to do XMLHttpRequest.
-//var nodes = [{this is json}].
-//everything between the brackets is json.
-var nodes = {
-
-};
